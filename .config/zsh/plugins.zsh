d71fd9487644adca21bee73bd357c4c0259710d7
gallon <h1090703848@gmail.com>
2021-01-05 23:15:57 +0800

    add zsh plugins update

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -3,27 +3,9 @@ if [[ ! -d $PLUG_DIR ]]; then
     curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh | bash
 fi
 
-
 # After automatic unpacking it provides program "fzf".
 zinit ice from"gh-r" as"program"
 zinit load junegunn/fzf-bin
-
-zinit ice atclone"dircolors -b LS_COLORS > clrs.zsh" \
-    atpull'%atclone' pick"clrs.zsh" nocompile'!' \
-    atload'zstyle ":completion:*" list-colors “${(s.:.)LS_COLORS}”'
-zinit light trapd00r/LS_COLORS
-
-zinit light-mode for \
-    zsh-users/zsh-autosuggestions \
-    zdharma/fast-syntax-highlighting \
-    zdharma/history-search-multi-word \
-    paulirish/git-open \
-    Aloxaf/fzf-tab \
-    skywind3000/z.lua \
-    kazhala/dotbare
-
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#9B30FF"
-
 
 # OMZ plugins
 zinit snippet OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh
@@ -31,3 +13,18 @@ zinit snippet OMZ::plugins/sudo/sudo.plugin.zsh
 
 zinit ice git
 zinit snippet OMZ::plugins/extract
+
+zinit light-mode for \
+    zdharma/history-search-multi-word \
+    paulirish/git-open \
+    skywind3000/z.lua \
+    kazhala/dotbare     \
+    Aloxaf/fzf-tab
+
+zinit wait lucid light-mode for \
+  atinit"zicompinit; zicdreplay" \
+      zdharma/fast-syntax-highlighting \
+  atload"_zsh_autosuggest_start" \
+      zsh-users/zsh-autosuggestions \
+
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#9B30FF"
--- .config/zsh/zshrc.zsh
+++ .config/zsh/zshrc.zsh
@@ -1,8 +1,11 @@
 source $HOME/.config/zsh/env.zsh
 source $HOME/.config/zsh/aliases.zsh
 source $HOME/.config/zsh/plugins.zsh
-source $HOME/.config/zsh/completion.zsh
 source $HOME/.config/zsh/fzf.zsh
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
+export STARSHIP_BIN=/usr/local/bin/starship
+if [[ ! -x $STARSHIP_BIN ]]; then
+    curl -fsSL https://starship.rs/install.sh | bash
+fi
 eval "$(starship init zsh)"
